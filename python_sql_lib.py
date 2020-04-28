8905fdb44e65c4dae71d4c30dbfa633aa5dfb65a
Colin Starger <colinstarger@gmail.com>
2020-04-27 21:00:42 -0400

    Take out unneeded function

--- python_sql_lib.py
+++ python_sql_lib.py
@@ -24,45 +24,6 @@ def excuteGenericFileQueryExportResults(dbconnection, sql_file, output_file):
 	df.to_csv(output_file)
 	
 
-def createCircuitMegaPandasGeneric(county, sql_file, output_file, begin_year, end_year, num_date_queries=1):
-
-	CIRCUIT = circuit_db[county]
-	dbconnection = mysql.connector.connect( host=hostname, user=username, passwd=password, db=CIRCUIT)
-	
-	begin = datetime.datetime(begin_year,1,1)
-	end = datetime.datetime(end_year, 12, 31)
-	print("Excuting making circuit mega Pandas for", output_file)
-
-	output_path = "../NewData/"
-	output_file = output_path + output_file
-
-	sqlStr = open(sql_file).read()
-	print("Opened SQL file", sql_file)
-	#dbcursor = myCircuitConnection.cursor(dictionary=True)
-	dbcursor = dbconnection.cursor(dictionary=True)
-	
-	#Just going to do this ugly
-	if (num_date_queries==1):
-		dbcursor.execute(sqlStr, (begin, end))
-	elif (num_date_queries==2):
-		dbcursor.execute(sqlStr, (begin, end, begin, end))
-	elif (num_date_queries==3):
-		dbcursor.execute(sqlStr, (begin, end, begin, end, begin, end))
-	elif (num_date_queries==4):
-		dbcursor.execute(sqlStr, (begin, end, begin, end, begin, end, begin, end))
-	else:
-		#assume 5 is limit!
-		dbcursor.execute(sqlStr, (begin, end, begin, end, begin, end, begin, end, begin, end))
-	print("Executed SQL")
-	results = dbcursor.fetchall()
-	print("Fetched all")
-	dbcursor.close()
-	df = pd.DataFrame(results)
-	print("Converted to dataframe")
-	df.to_csv(output_file)
-
-	dbconnection.close()
-
 
 def openIDB_connection():
 	dbconnection = mysql.connector.connect( host=fjc_host, user=fjc_colin, passwd=fjc_colin_password, db=fjc_db)
